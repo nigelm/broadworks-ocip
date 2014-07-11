98e30ddbe0890553558d4e079587f932e453480b
Nigel Metheringham <nigel@dotdot.it>
2014-07-11 17:26:23 +0100

    Added some documentation and decoration

--- lib/Broadworks/OCIP/Response.pm
+++ lib/Broadworks/OCIP/Response.pm
@@ -17,7 +17,50 @@ use MooseX::StrictConstructor;
 use Try::Tiny;
 use XML::BareX;
 
+=begin :prelude
+
+=for test_synopsis
+1;
+__END__
+
+=for stopwords NIGELM 
+
+=for Pod::Coverage mvp_multivalue_args
+
+=end :prelude
+
+=head1 SYNOPSIS
+
+  use Broadworks::OCIP::Response;
+
+  my $res = Broadworks::OCIP::Response->new(
+    xml => string,
+    expected => 'SuccessResponse' );
+  $res->status_ok;
+
+=head1 DESCRIPTION
+
+Unpacks an XML document sent as a reply and allows its manipulation.
+
+=head2 Required Parameters
+
+=cut
+
 # ------------------------------------------------------------------------
+sub _list {
+    return () unless ( defined( $_[0] ) );
+    return @{ $_[0] } if ( ref( $_[0] ) eq 'ARRAY' );
+    return $_[0];
+}
+
+# ------------------------------------------------------------------------
+
+=head3 xml
+
+The XML document returned.  This should be a perl string - not encoded.
+
+=cut
+
 has xml => (
     is       => 'ro',
     isa      => 'Str',
@@ -25,6 +68,15 @@ has xml => (
 );
 
 # ------------------------------------------------------------------------
+
+=head3 expected
+
+The response type we are expecting.  This is used to populate C<status_ok>. If
+C<die_on_error> is set an exception will be thrown if the received document is
+not of this type.
+
+=cut
+
 has expected => (
     is       => 'ro',
     isa      => 'Str',
@@ -32,6 +84,13 @@ has expected => (
 );
 
 # ------------------------------------------------------------------------
+
+=head3 die_on_error
+
+Whether we throw toys out of pram on unexpected input.
+
+=cut
+
 has die_on_error => (
     is       => 'ro',
     isa      => 'Bool',
@@ -40,6 +99,13 @@ has die_on_error => (
 );
 
 # ------------------------------------------------------------------------
+
+=head3 hash
+
+A perl hash representation of the XML.
+
+=cut
+
 has hash => (
     is      => 'ro',
     isa     => 'HashRef',
@@ -66,6 +132,13 @@ method _build_hash () {
 }
 
 # ------------------------------------------------------------------------
+
+=head3 type
+
+The type of the returned XML.
+
+=cut
+
 has type => (
     is      => 'ro',
     isa     => 'Str',
@@ -75,6 +148,13 @@ has type => (
 method _build_type () { return ( $self->payload->{'xsi:type'} ); }
 
 # ------------------------------------------------------------------------
+
+=head3 status_ok
+
+Is this the expected return type.
+
+=cut
+
 has status_ok => (
     is      => 'ro',
     isa     => 'Bool',
@@ -84,6 +164,13 @@ has status_ok => (
 method _build_status_ok () { return ( ( $self->type eq $self->expected ) ? 1 : 0 ); }
 
 # ------------------------------------------------------------------------
+
+=head3 payload
+
+The hash payload of the XML document.
+
+=cut
+
 has payload => (
     is      => 'ro',
     isa     => 'HashRef',
@@ -93,11 +180,21 @@ has payload => (
 method _build_payload () { return $self->hash->{command}; }
 
 # ------------------------------------------------------------------------
+
+=head3 payload
+
+Any tables that are in the returned data.  This returns a hash of tables named
+by the table name.
+
+Each table is an array of rows, each of which is a hash.
+
+=cut
+
 has tables => (
     is      => 'ro',
     isa     => 'HashRef',
     lazy    => 1,
-    builder => '_build_hash'
+    builder => '_build_tables'
 );
 
 method _build_tables () {
@@ -106,12 +203,13 @@ method _build_tables () {
         if ( $k =~ /Table$/ ) {
             my $table = [];
             $tables->{$k} = $table;
-            my @cols = list( $v->{colHeading} );
-            foreach my $row ( list( $v->{row} ) ) {
+            my @cols = _list( $v->{colHeading} );
+            foreach my $row ( _list( $v->{row} ) ) {
                 push( @{$table}, { map { $cols[$_] => $row->{col}->[$_] } 0 .. $#cols } );
             }
         }
     }
+    return $tables;
 }
 
 # ------------------------------------------------------------------------
@@ -130,3 +228,15 @@ method BUILD ($args) {
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Nigel Metheringham <Nigel.Metheringham@redcentricplc.com>
+
+=head1 COPYRIGHT
+
+Copyright 2014 Recentric Solutions Limited. All rights reserved.
+
+=cut
--- lib/Broadworks/OCIP/Throwable.pm
+++ lib/Broadworks/OCIP/Throwable.pm
@@ -6,11 +6,50 @@ use strict;
 use warnings;
 use utf8;
 use namespace::autoclean;
-use Moose;
-extends 'Throwable::Error';
 
 # VERSION
 # AUTHORITY
+
+use Moose;
+extends 'Throwable::Error';
+
+=begin :prelude
+
+=for test_synopsis
+1;
+__END__
+
+=for stopwords NIGELM 
+
+=for Pod::Coverage mvp_multivalue_args
+
+=end :prelude
+
+=head1 SYNOPSIS
+
+  use Broadworks::OCIP::Throwable;
+
+  Broadworks::OCIP::Throwable->throw(...);
+
+=head1 DESCRIPTION
+
+Throws an exception.
+
+=head2 Required Parameters
+
+=head3 message
+
+An exception message.
+
+=head3 execution_phase
+
+Text to give an indication of what we were doing.
+
+=head3 error_code
+
+Text to uniquely identify what caused this.
+
+=cut
 
 # ------------------------------------------------------------------------
 
@@ -29,3 +68,15 @@ has error_code => (
 # ------------------------------------------------------------------------
 __PACKAGE__->meta->make_immutable;
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Nigel Metheringham <Nigel.Metheringham@redcentricplc.com>
+
+=head1 COPYRIGHT
+
+Copyright 2014 Recentric Solutions Limited. All rights reserved.
+
+=cut
